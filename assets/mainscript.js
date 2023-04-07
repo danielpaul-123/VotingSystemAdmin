0eafa47c5c23a3a03fe24d5b63afa16daa6f4dfb
danielpaul-123 <danielpaulperinchery07012005@gmail.com>
2023-04-07 19:34:28 +0530

    Main Web App for issuing New Electoral IDs

--- assets/mainscript.js
+++ assets/mainscript.js
@@ -1,47 +1,128 @@
 const bk = document.getElementById('beck');
-const addvote = document.getElementById('addvote');
-const thumbnails = document.getElementById('thumb');
-const cancel = document.getElementById('cancel');
-const create = document.getElementById('create');
+const submiter = document.getElementById('submit');
+const form = document.querySelector("form");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCjkXPnUijS3uYWvuPheSjJeFvoiZ7nrQ0",
+  authDomain: "election-online-prj.firebaseapp.com",
+  databaseURL: "https://election-online-prj-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "election-online-prj",
+  storageBucket: "election-online-prj.appspot.com",
+  messagingSenderId: "281761267368",
+  appId: "1:281761267368:web:fb0663844a76d66df40c9d"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 bk.addEventListener("click",function(event){
   event.preventDefault();
   window.location.replace("../index.html");
 })
 
-addvote.addEventListener("click",function (event) {
-  var rform = document.querySelector('.form-box');
-  var blur = document.querySelector('.bgform');
-  rform.style.display = 'inline-flex'
-  blur.style.display = 'flex';
+submiter.addEventListener("click",async function(event){
+  event.preventDefault();
+
+  //Read from firebase code, compare keyword with argon2id hash to login
+  // argon2.hash({
+  //   pass:"dibin",
+  //   salt:random(16),
+  //   time:2,
+  //   mem:16384,
+  //   hashLen:32,
+  //   parallelism:1,
+  //   type:argon2.ArgonType.Argon2id,
+  //   }).then(h => console.log("Username: ",h.encoded))
+  //   .catch(e => console.error(e.message, e.code));
+  
+  var uname = document.getElementById("unm").value;
+
+  argon2.hash({
+    pass:uname,
+    salt:random(16),
+    time:2,
+    mem:16384,
+    hashLen:32,
+    parallelism:1,
+    type:argon2.ArgonType.Argon2id,
+    }).then(h => {const hashuname = h.encoded;
+      argonpass(hashuname);
+      }).catch(e => console.error(e.message, e.code));
 })
 
-function canceller(event) {
-  var rform = document.querySelector('.form-box');
-  var blur = document.querySelector('.bgform');
-  rform.style.display = 'none';
-  blur.style.display = 'none';
+function argonpass(_hashuname)
+{
+  const hashuname = _hashuname;
+  var pass = document.getElementById("pwd").value;
+  argon2.hash({
+    pass:pass,
+    salt:random(16),
+    time:2,
+    mem:16384,
+    hashLen:32,
+    parallelism:1,
+    type:argon2.ArgonType.Argon2id,
+    }).then(h => {const hashpass = h.encoded;
+    firestoreupload(hashuname,hashpass);
+    }).catch(e => console.error(e.message, e.code));
 }
 
-create.addEventListener('click',function (event) {
-  var election = document.getElementById('eleccat').value;
-  var newBox = document.createElement('div');
-  newBox.setAttribute('class','box');
+function firestoreupload(_hashuname,_hashpass)
+{
+  var vid = document.getElementById("vid").value;
+  const hashuname = _hashuname;
+  const hashpass = _hashpass;
+  const firestore = firebase.firestore();
+  var collectionref = firestore.collection("Test_User").doc(vid);
 
-  var newInner = document.createElement('div');
-  newInner.setAttribute('class','inner');
+  const data = {
+    Username: hashuname,
+    Password: hashpass,
+  };
 
-  var newH = document.createElement('h2');
-  newH.innerHTML= election;
-
-  var newButton = document.createElement('a');
-  newButton.setAttribute('class','button fit')
-  newButton.innerHTML='View';
-
-  canceller(event);
-
-  newInner.appendChild(newH);
-  newInner.appendChild(newButton);
-  newBox.appendChild(newInner);
-  thumbnails.appendChild(newBox);
+collectionref.set(data)
+.then(() => {
+  electionupdate(vid);
+  imageupload(vid);
+  form.reset();
 })
+.catch((error) => {
+  console.error("Error adding document: ", error);
+});
+}
+
+function electionupdate (_voterid)
+{
+  var voterid = _voterid;
+  const firestore = firebase.firestore();
+  var voterdocument = firestore.collection("Test_User").doc(voterid);
+  var electioncollection = firestore.collection("Election_Data");
+
+  electioncollection.get().then((querySnapshot) => {
+    // Loop through the documents
+    querySnapshot.forEach((doc) => {
+      // doc.data() is the document contents
+      const docid = doc.id;
+      const data = {
+        [docid]: 0,
+      }
+
+      voterdocument.update(data)
+      .then(() => {
+        form.reset();
+      })
+      .catch((error) => {
+        console.error("Error adding document: ", error);
+      });
+    });
+  });
+}
+
+function imageupload(_voterid)
+{
+  let voterid = _voterid+".jpg";
+  const profile = document.getElementById("pimg").files[0];
+  const storageRef = firebase.storage().ref().child('user_profile/'+voterid);
+  storageRef.put(profile).then((snapshot) => {
+    console.log('Succesfull!');
+  })
+}
--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,97 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCjkXPnUijS3uYWvuPheSjJeFvoiZ7nrQ0",
+  authDomain: "election-online-prj.firebaseapp.com",
+  databaseURL: "https://election-online-prj-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "election-online-prj",
+  storageBucket: "election-online-prj.appspot.com",
+  messagingSenderId: "281761267368",
+  appId: "1:281761267368:web:fb0663844a76d66df40c9d"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+var id = 1;
+
 const skipp = document.getElementById('skp');
 const headers = document.querySelectorAll('th');
 const err = document.getElementById('err');
 const submiter = document.getElementById('submit');
+
 let currentHeader = 0;
 
 highligter();
-
+var a=0;
 //hides the skip button when pressed
-skipp.onclick = function(){
-  skipp.style.display='none';
+skipp.onclick = function()
+{
+  a++;
+  if(a>2)
+  {
+    id++;
+    skipp.style.display='none';
+  }
+  else
+  {
+    id++;
+  }
   err.style.display = "none";
   highligter();
   form.reset();
 };
 
 //formSumbitHandler
-submiter.addEventListener("click",function(event){
+submiter.addEventListener("click",async function(event){
   event.preventDefault();
-  
-  //for test purposes checks pass and uname were hello or not ,later replace with proper firebasecode
-  var name = document.getElementById("unm").value;
-  var pass = document.getElementById("pass").value;
-  if(name != "user" || pass != "pass")
-  err.style.display = "block";//if login is unsuccesfull throws a error message
-  
-  //else clears the form and highlights next admins name
-  else{
-    highligter();
-  }
+
+  //Read from firebase code, compare keyword with argon2id hash to login
+  // argon2.hash({
+  //   pass:"dibin",
+  //   salt:random(16),
+  //   time:2,
+  //   mem:16384,
+  //   hashLen:32,
+  //   parallelism:1,
+  //   type:argon2.ArgonType.Argon2id,
+  //   }).then(h => console.log("Username: ",h.encoded))
+  //   .catch(e => console.error(e.message, e.code));
+          
+    let adminid = "Admin"+id;
+    const firestore = firebase.firestore();
+    var docref = firestore.collection("Admin_User").doc(adminid);
+    docref.get().then((doc) => {
+      if(doc.exists)
+      {
+        let uname = doc.data().Username;
+        let pswd = doc.data().Password;
+        var name = document.getElementById("unm").value;
+        var pass = document.getElementById("pwd").value;
+        
+        argon2.verify({pass:name, encoded:uname})
+        .then(() =>{
+
+          argon2.verify({pass:pass, encoded:pswd})
+          .then(() => {
+
+            id++;
+            highligter();
+
+          }).catch(e => err.style.display = "block");
+
+        }).catch(e => err.style.display = "block");
+      }
+      else
+      {
+        console.log("No Document!");
+      }
+    }).catch((error) => {
+      console.log("Error!");
+      console.log(error);
+    });
+        
 })
 
 //function to highlight each admin's name on a succesfull login and a skip
 function highligter(){
-
   err.style.display = "none";
   if(currentHeader > 0){
     headers[currentHeader -1].classList.remove('foc');
@@ -44,12 +103,7 @@ function highligter(){
   }
   
   headers[currentHeader].classList.add('foc');
-  /* var x  = document.getElementsById('jaba');
-  var d = x.innerHTML;
-  console.log(x);
-  console.log(d);
-  this.window.alert("hi") */
   currentHeader++;
   document.getElementById("unm").value = "";
-  document.getElementById("pass").value = "";
+  document.getElementById("pwd").value = "";
 }
